dc250b398dc2d33f4f2918e183d4ecea1447fcf3
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2022-04-04 13:58:46 +0200

    tests: basic HSpec tests

--- test/CaesarTest.hs
+++ test/CaesarTest.hs
@@ -1,12 +1,15 @@
 module CaesarTest where
 
+import           Test.Hspec                    as H
 import           Test.Tasty
+import           Test.Tasty.Hspec
 import           Test.Tasty.QuickCheck
 
 import           Caesar.Core                   as Caesar
 
 arbStr :: Gen String
 arbStr = listOf $ elements alpha where alpha = ['a' .. 'z']
+
 arbTxt :: Gen Caesar.Text
 arbTxt = do
   Caesar.Text <$> arbStr
@@ -14,7 +17,23 @@ arbTxt = do
 instance Arbitrary Caesar.Text where
   arbitrary = arbTxt
 
-prop_encryptDecrypt :: Caesar.Text -> Int -> Bool
-prop_encryptDecrypt text key =
+prop_encryptDecryptGeneratedTests :: Caesar.Text -> Int -> Bool
+prop_encryptDecryptGeneratedTests text key =
   Caesar.decrypt (Caesar.encrypt text key) key == text
+
+spec_encrypt :: Spec
+spec_encrypt = describe "Caesar.encrypt" $ do
+  it "Plaintext: 'irma' Key: 3 should return Ciphertext: 'lupd'" $ do
+    Caesar.encrypt (Caesar.Text "irma") 3 `shouldBe` Caesar.Text "lupd"
+
+  it "Plaintext: 'zzzz' Key: 1 should return Ciphertext: 'lupd'" $ do
+    Caesar.encrypt (Caesar.Text "zzzz") 1 `shouldBe` Caesar.Text "aaaa"
+
+spec_decrypt :: Spec
+spec_decrypt = describe "Caesar.decrypt" $ do
+  it "Ciphertext: 'lupd' Key: 3 should return Plaintext: 'irma'" $ do
+    Caesar.decrypt (Caesar.Text "lupd") 3 `shouldBe` Caesar.Text "irma"
+
+  it "Ciphertext: 'aaaa' Key: 1 should return Plaintext: 'zzzz'" $ do
+    Caesar.decrypt (Caesar.Text "aaaa") 1 `shouldBe` Caesar.Text "zzzz"
 
