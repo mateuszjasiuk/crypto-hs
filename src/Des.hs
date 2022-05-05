df5ddddbd79071b0fc95362b885fb07b094480f5
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2022-05-05 09:54:55 +0200

    feat: key parity drop

--- src/Des.hs
+++ src/Des.hs
@@ -2,21 +2,19 @@ module Des
   ( encrypt
   , permutate
   , drop8thBits
+  , parityDrop
+  , splitKey
   ) where
 
 import           Data.Bits
 import           Data.Word
 
-initialPermIdx :: [(Int, Int)]
-initialPermIdx = zip initialPerm [0 .. 63]
-
 -- 0b01111111
 mask :: Word64
 mask = 0x7F
 
--- 0b10000000000000000000000000001111
 key :: Word64
-key = 0x800000000000000F
+key = 0xFFFFFFFFFFFFFFFF
 
 drop8thBit :: Word64 -> Word64 -> Int -> Word64
 drop8thBit key a c = a .|. (key .&. mask `shiftL` (c * 8)) `shiftR` 1
@@ -25,19 +23,28 @@ drop8thBits :: Word64 -> Word64
 drop8thBits key =
   key .&. mask .|. foldl (drop8thBit key) (0 :: Word64) [1 .. 7]
 
-finalPermIdx :: [(Int, Int)]
-finalPermIdx = zip finalPerm [0 .. 63]
+permToMap :: [Int] -> [(Int, Int)]
+permToMap p = zip p [0 .. (length p)]
 
-permutate :: Word64 -> (Int, Int) -> Word64
-permutate plaintext (x, y) = shift (plaintext .&. bit y) (x - 1 - y)
+permutateOne :: Word64 -> (Int, Int) -> Word64
+-- (x - 1) and (y - x + 1) because haskell indexes from 0 and perm tables index from 1
+permutateOne word (x, y) = (word .&. bit (x - 1)) `shift` (y - x + 1)
+
+permutate :: Word64 -> [Int] -> Word64
+permutate word p = foldr (xor . permutateOne word) (0 :: Word64) (permToMap p)
+
+parityDrop :: Word64 -> Word64
+parityDrop key = permutate key keyp
+
+-- TODO: Change res to (Word32, Word32)
+splitKey :: Word64 -> (Word64, Word64)
+splitKey key = (key .&. 0xFFFFFFF, (key .&. 0xFFFFFFF0000000) `shiftR` 28)
 
 encrypt :: Word64 -> Word64
-encrypt plaintext =
-  foldr (xor . permutate plaintext) (0 :: Word64) initialPermIdx
+encrypt plaintext = permutate plaintext initialPerm
 
 decrypt :: Word64 -> Word64
-decrypt ciphertext =
-  foldr (xor . permutate ciphertext) (0 :: Word64) finalPermIdx
+decrypt ciphertext = permutate ciphertext finalPerm
 
 initialPerm =
   [ 58
@@ -171,4 +178,62 @@ finalPerm =
   , 17
   , 57
   , 25
+  ]
+keyp =
+  [ 57
+  , 49
+  , 41
+  , 33
+  , 25
+  , 17
+  , 9
+  , 1
+  , 58
+  , 50
+  , 42
+  , 34
+  , 26
+  , 18
+  , 10
+  , 2
+  , 59
+  , 51
+  , 43
+  , 35
+  , 27
+  , 19
+  , 11
+  , 3
+  , 60
+  , 52
+  , 44
+  , 36
+  , 63
+  , 55
+  , 47
+  , 39
+  , 31
+  , 23
+  , 15
+  , 7
+  , 62
+  , 54
+  , 46
+  , 38
+  , 30
+  , 22
+  , 14
+  , 6
+  , 61
+  , 53
+  , 45
+  , 37
+  , 29
+  , 21
+  , 13
+  , 5
+  , 28
+  , 20
+  , 12
+  , 4
   ]
